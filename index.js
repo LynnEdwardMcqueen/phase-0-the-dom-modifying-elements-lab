cae3663f6255eccc696a31eb4c0d7a015bc0f696
Lynn Edward McQueen <lynn.edward.mcqueen@gmail.com>
2023-03-12 12:38:52 -0600

    adds DOM processing JavaScript

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const mainClassMain = document.querySelector('main#main');
+mainClassMain.remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+newHeader.innerHTML = "Lynn McQueen is the champion"
+
+
